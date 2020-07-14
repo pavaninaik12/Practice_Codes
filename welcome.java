31ba413f65c4e330baf81219608d85460334e656
Pavani <57756007+pavaninaik12@users.noreply.github.com>
2020-07-14 11:15:43 +0530

    update 2

--- welcome.java
+++ welcome.java
@@ -2,5 +2,7 @@ import java.io.*;
 
 public static void main(String args[])
 { System.out.println("Welcome to Java");
- System.out.println("Thank you");
+  System.out.println("Thank you");
+  System.out.println(" :) ");
+ 
 }
