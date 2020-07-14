b38d2446ae31726da2fc2dc8a0e7d18e3fd4e1bb
Pavani <57756007+pavaninaik12@users.noreply.github.com>
2020-07-14 11:23:28 +0530

    new_change

--- welcome.java
+++ welcome.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 { System.out.println("Welcome to Java");
   System.out.println("Thank you");
   System.out.println(" :) ");
- 
+  int a,b;
+  System.out.println("SUM:"+(a+b));
 }
